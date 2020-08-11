9eedcaa99267051cd463d14af93d0f9d0043c584
sarahsmi <68827231+sarahsmi@users.noreply.github.com>
2020-08-12 09:35:06 +1200

    version 1
    website pages created and linked to the stylesheet (which isn't displayed as it is currently empty)

---
+++ order.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Order - WGC Canteen</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" href="https://wgc.school.nz/wp-content/uploads/2018/09/WGC_Logo_Transparent_RGB.png" type="image/gif">
+</head>
+</html>
